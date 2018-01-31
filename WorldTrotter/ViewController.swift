51e8e30a954953933486aa62b72d3b1793632f15
Button IV <001325415@N101-A8-Mac.local>
2018-01-30 19:29:36 -0500

    first commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Button IV, William J on 1/30/18.
+//  Copyright © 2018 Button IV, William J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ /*   override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstFrame = CGRect(x: 160, y: 140, width: 100, height: 150)
+        
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //view.addSubview(secondView)
+        firstView.addSubview(secondView)
+    }
+
+   */
+
+}
+
